675fac5f12ae1d0d4901d60ba1abd4b3c1d17f69
Dragan Espenschied <drx@a-blast.org>
2012-04-04 12:21:28 +0200

    protocoll of last execution time

--- scripts/geocities.archiveteam.torrent/004-resolve-symlinks-01.sh
+++ scripts/geocities.archiveteam.torrent/004-resolve-symlinks-01.sh
@@ -6,3 +6,7 @@
 cd $GEO_WORK/geocities/www.geocities.com
 
 find . -type l -print0 | xargs -0 -I filename bash -c 'echo -n "filename "; readlink filename;' | sed 's|^\(.\+\) \(.\+\)|rm -v \1 ; mv -v \2 \1|g' | tr '\n' '\0' | xargs -0 -n 1 bash -c
+
+# real    49m22.583s
+# user    0m4.064s
+# sys     0m26.986s
