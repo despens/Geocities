35e4a6c3ccee773f66ae52abfc999c0da06c0657
Dragan Espenschied <despens@eiger>
2012-12-25 19:29:35 +0100

    Made much faster by replacing find with ls.

--- scripts/geocities.archiveteam.torrent/002-untar.sh
+++ scripts/geocities.archiveteam.torrent/002-untar.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# The torrent holds some 7zip archives that already contain real directories,
-# most contain tarballs. They will be unpacked and deleted after 
-# successful unpacking.
+# The torrent holds some 7zip archives that already contain
+# real directories, most contain tarballs. They will be 
+# untared and deleted after.
 
 cd $GEO_WORK
 
-find . -type f  -print0 | xargs -0 file -i | grep -i application/x-tar | sed 's/:.\+//' | tr '\n' '\0' | xargs -I filename -0 -n 1 bash -c 'tar xvf filename && rm -v filename'
-# find files              run the "file"     "grep" filters out all      Throwing away    Convert new-   Run bash to execute the untar operation             ^^^^^^^^^^^^^^^^^
-# and send them to        command on each    files that are not          any output but   lines to       and delete the tarballs after successful            Remove this if you
-# xargs separated by      found file to      tarballs.                   the file names.  NULL bytes     decrunching.                                        would like to keep
-# NULL bytes              determine its                                                   so xargs can                                                       copies of the
-#                         mime-type                                                       pass each                                                          decrunched files!
-#                                                                                         filename as 
+ls -f -1 | tr '\n' '\0' | xargs -0 file -i | grep -i application/x-tar | sed 's/:.\+//' | tr '\n' '\0' | xargs -I filename -0 -n 1 bash -c 'tar xvf filename && rm -v filename'
+# list all    Separate    Run the "file"     "grep" finds all the files  Throwing away    Convert new-   Run bash to execute the untar operation             ^^^^^^^^^^^^^^^^^
+# files in    lines by    command on each    that are tarballs.          any output but   lines to       and delete the tarballs after successful            Remove this if you
+# the dir,    NULL bytes  found file to                                  the file names.  NULL bytes     decrunching.                                        would like to keep
+# just name   for xargs.  determine its                                                   so xargs can                                                       copies of the
+# and one                 mime-type                                                       pass each                                                          decrunched files!
+# column.                                                                                 filename as 
 #                                                                                         an argument
+
 
 #real    519m26.836s
 #user    28m24.299s
